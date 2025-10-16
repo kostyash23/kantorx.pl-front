215a80beeb95ee3bf1224cf61cb7c332d6e5046b
kshmotolokha <kostiashmotoloha@gmail.com>
2025-10-16 12:53:07 +0200

    change lonk for footer telegram

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -11,9 +11,9 @@ const Contacts = () => {
         <div className="flex lg:flex-col flex-wrap md:flex-row flex-col gap-4 2xl:w-[500px] lg:w-[360px] w-full">
           <a
             className="bg-white text-center font-semibold text-black px-2 py-4 rounded-[5px]"
-            href="href='https://t.me/kantorXFabrykaNorblina'"
+            href="https://t.me/kantorXFabrykaNorblina"
           >
-            Kontakt na Telegramie
+            {t('telegram_contact')}
           </a>
 
           <div className="flex items-center justify-start p-4 rounded-[5px] leading-[26px] bg-[#060606] w-full lg:text-base text-sm">
